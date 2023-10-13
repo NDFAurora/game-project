aef71e9bc3727c56ae3016d73909dd58eaceae62
mathdubois <math94.dubois@outlook.fr>
2023-10-13 10:51:16 +0200

    Initial commit

---
+++ My project/Assets/Code/GameManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ground;
+    private float period = 4;
+    public float timer = 0;
+    void Start()
+    {
+        ground = GameObject.Find("Ground");
+        if(ground == null )
+        {
+            throw new MissingReferenceException("Ground plane missing");
+        }
+
+        GameObject firstGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        firstGameObject.AddComponent<Rigidbody>();
+        firstGameObject.transform.localScale = Vector3.one;
+        firstGameObject.transform.localPosition = new Vector3(0f, 3f, 0f);
+    }
+
+    void Update()
+    {
+        if(timer < period)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+        GroundSineRotation();
+    }
+
+    // Rotate ground plane from 30° to -30° every <period>seconds
+    void GroundSineRotation()
+    {
+        float amplitude = .5f;
+        float motive = (float) (2 * Math.PI / 4);
+        float phase = 1f;
+
+        float sine = amplitude * Mathf.Sin(motive * (timer - phase));
+        Debug.Log($"{sine} at {timer}");
+
+        ground.transform.Rotate(new Vector3(0f,0f, sine/30.0f), Space.World);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # game-project
+
+## Convention de nommage
+
+- Variables
+    - locales   : `myVar`
+    - globales  : `MYVAR`
+- Paramètres de fonctions : `p_myParam`
+- Attribut de classe privé: `_myAttribute`
+- Attribut de classe publique: `a_myAttribute`
+- Classe, Fonction, Espace de nom: `MyClass`
